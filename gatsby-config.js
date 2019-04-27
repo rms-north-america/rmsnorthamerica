e5c43990c0af9fe75d35414a0aa54c35b159fbf0
jokokoloko <jhowland82@gmail.com>
2019-04-27 07:52:45 -0700

    Added gatsby-source-contentful package

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
     siteMetadata: {
         title: 'Ghost',
     },
-    plugins: ['gatsby-plugin-react-helmet'],
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-source-contentful',
+            options: {
+                spaceId: process.env.CONTENTFUL_SPACE_ID,
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+            },
+        },
+    ],
 };
