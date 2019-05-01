e713a93d94076327f846fe183a0bf078e182fdeb
jokokoloko <jhowland82@gmail.com>
2019-05-01 03:10:51 -0700

    Added gatsby-image, gatsby-transformer-sharp, and gatsby-plugin-sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     },
     plugins: [
         'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sharp',
         {
             resolve: 'gatsby-source-contentful',
             options: {
@@ -14,5 +15,6 @@ module.exports = {
             },
         },
         'gatsby-transformer-remark',
+        'gatsby-transformer-sharp',
     ],
 };
