a9ba78c3c0682c4017cac4ce2d74e6302ad4ab36
jokokoloko <jhowland82@gmail.com>
2019-05-07 00:42:39 -0700

    Modified index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,31 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import Basic from '../components/section/Basic';
+import Hero from '../components/section/Hero';
+import ButtonMain from '../components/project/ButtonMain';
 
 export default ({ location, data }) => {
     const { introduction } = data;
     return (
         <Layout template="home" location={location}>
+            <Hero id="splash" color={3}>
+                <div className="row align-items-center gutter-80">
+                    <div className="col">
+                        <header className="node-xs-50">
+                            <h1>The Hospitality Cloud</h1>
+                            <h2>
+                                A fully integrated Cloud Based Property Management System.
+                                <br />
+                                The perfect, seamless solution for your hospitality needs.
+                            </h2>
+                        </header>
+                        <section className="node-xs-50">
+                            <ButtonMain label="Request a demo" />
+                        </section>
+                    </div>
+                    <div className="col">2 of 2</div>
+                </div>
+            </Hero>
             {introduction && (
                 <Basic id={introduction.slug} space="space-xs-80 space-lg-130">
                     <header dangerouslySetInnerHTML={{ __html: introduction.body.childMarkdownRemark.html }} />
