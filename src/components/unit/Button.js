a99aed4a7a2e74ff603099b27203d6d191375130
jokokoloko <jhowland82@gmail.com>
2019-05-19 05:03:38 -0700

    Changed ButtonMain.js to Button.js

---
+++ src/components/unit/Button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+
+const Button = ({ label, kind, size, display, to, disabled }) => (
+    <Link className={`btn btn-${kind} btn-${size} btn-${display}`} to={to} disabled={disabled}>
+        {label}
+    </Link>
+);
+
+Button.propTypes = {
+    label: PropTypes.string,
+    kind: PropTypes.string,
+    size: PropTypes.string,
+    display: PropTypes.string,
+    to: PropTypes.string,
+    disabled: PropTypes.bool,
+};
+
+Button.defaultProps = {
+    label: 'Submit',
+    kind: 'default',
+    size: 'lg',
+    display: 'initial',
+    to: '/',
+    disabled: false,
+};
+
+export default Button;
