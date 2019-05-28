55f0ff042c080be148508ec417a496333abce235
jokokoloko <jhowland82@gmail.com>
2019-05-27 20:22:26 -0700

    Added request-demo.js

--- src/pages/request-demo.js
+++ src/pages/request-demo.js
@@ -4,18 +4,16 @@ import Layout from '../components/Layout';
 import Basic from '../components/section/Basic';
 
 export default ({ location, data }) => {
-    const { page } = data;
+    const { page, form } = data;
     const description = page.excerpt ? page.excerpt.excerpt : page.body.childMarkdownRemark.excerpt.replace(/\n/g, ' ');
     return (
         <Layout template="single single-page" title={page.title} description={description} location={location}>
             <Basic id={page.slug} space="space-xs-50 space-lg-80">
-                <header className="node-xs-30 node-lg-50 text-lg-center">
-                    <h1>{page.title}</h1>
-                </header>
-                <section
+                <header
                     className="node-xs-30 node-lg-50 text-lg-center width"
-                    dangerouslySetInnerHTML={{ __html: page.body.childMarkdownRemark.html.replace(/\n/g, '<br>').replace(/<br><p>/g, '<p>') }}
+                    dangerouslySetInnerHTML={{ __html: page.body.childMarkdownRemark.html }}
                 />
+                <footer className="node-xs-30 node-lg-50 text-lg-center width" dangerouslySetInnerHTML={{ __html: form.code.code }} />
             </Basic>
         </Layout>
     );
@@ -34,6 +32,11 @@ export const query = graphql`
             }
             excerpt {
                 excerpt
+            }
+        }
+        form: contentfulForm {
+            code {
+                code
             }
         }
     }
