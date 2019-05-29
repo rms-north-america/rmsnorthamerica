82acc80c242a80fdcaa27043ad32348d592ca3de
jokokoloko <jhowland82@gmail.com>
2019-05-28 18:20:37 -0700

    Minor change to request-demo.js

--- src/pages/request-demo.js
+++ src/pages/request-demo.js
@@ -8,13 +8,15 @@ export default ({ location, data }) => {
     const description = page.excerpt ? page.excerpt.excerpt : page.body.childMarkdownRemark.excerpt.replace(/\n/g, ' ');
     return (
         <Layout template="single single-page" title={page.title} description={description} location={location}>
-            <Basic id={page.slug} space="space-xs-50 space-lg-80">
-                <header
-                    className="node-xs-30 node-lg-50 text-lg-center width"
-                    dangerouslySetInnerHTML={{ __html: page.body.childMarkdownRemark.html }}
-                />
-                <footer className="node-xs-30 node-lg-50 text-lg-center width" dangerouslySetInnerHTML={{ __html: form.code.code }} />
-            </Basic>
+            {page && (
+                <Basic id={`basic-${page.slug}`} space="space-xs-50 space-lg-80">
+                    <header
+                        className="node-xs-30 node-lg-50 text-lg-center width"
+                        dangerouslySetInnerHTML={{ __html: page.body.childMarkdownRemark.html }}
+                    />
+                    <footer className="node-xs-30 node-lg-50 text-lg-center width" dangerouslySetInnerHTML={{ __html: form.code.code }} />
+                </Basic>
+            )}
         </Layout>
     );
 };
