ad1bedb9e0ca55341751d866c59580b7775d4e3f
jokokoloko <jhowland82@gmail.com>
2019-05-31 00:04:18 -0700

    Added Card.js

---
+++ src/components/unit/Card.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import Img from 'gatsby-image';
+
+const Card = ({ node, column, effect, item, path }) => (
+    <article id={`${item}-${node.slug}`} className={`${item} ${item}-${node.order} effect-image ${column}`}>
+        <figure className={`effect-${effect}`}>
+            <Img className="image fit" fluid={node.image.fluid} alt={node.title} />
+            <figcaption className="dark-30 d-flex align-items-center">
+                <h4 className="headline">{node.title}</h4>
+                <div className="caption">
+                    <p
+                        className="excerpt"
+                        dangerouslySetInnerHTML={{
+                            __html: node.excerpt ? node.excerpt.excerpt : node.body.childMarkdownRemark.excerpt,
+                        }}
+                    />
+                    <p className="action">{`${node.action || 'Learn more'} →`}</p>
+                </div>
+                <Link className="link" to={path ? `${path}/${node.slug}` : `/${node.slug}`}>
+                    view more
+                </Link>
+            </figcaption>
+        </figure>
+    </article>
+);
+
+Card.propTypes = {
+    node: PropTypes.object.isRequired,
+    column: PropTypes.string,
+    effect: PropTypes.string,
+    item: PropTypes.string,
+    path: PropTypes.string,
+};
+
+Card.defaultProps = {
+    column: 'col',
+    effect: 'oscar',
+    item: 'post',
+    path: undefined,
+};
+
+export default Card;
