9a7f13b187d5e70adb59e8bb9fa13e043bdfa86e
jokokoloko <jhowland82@gmail.com>
2019-06-05 06:22:15 -0700

    Minor change to 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ import Basic from '../components/section/Basic';
 
 export default ({ location }) => (
     <Layout template="not-found" title="404: Not Found" description="This page does not exist." location={location}>
-        <Basic id="not-found" space="space-xs-80 space-lg-130">
+        <Basic id="not-found" space="space-custom">
             <header>
                 <h3 className="p-xs-25">404: Not Found</h3>
             </header>
