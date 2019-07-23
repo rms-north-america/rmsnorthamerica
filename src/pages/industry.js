20f135dd375b84973c90ddb4bdf3589ed2543a5f
jokokoloko <jhowland82@gmail.com>
2019-07-23 12:01:56 -0700

    Updated industry.js to remove more "clients" code

--- src/pages/industry.js
+++ src/pages/industry.js
@@ -5,15 +5,13 @@ import * as path from '../path';
 import Layout from '../components/Layout';
 import Feed from '../components/section/Feed';
 import Card from '../components/unit/Card';
-import ArticleClient from '../components/project/ArticleClient';
 import GeneralRequestDemo from '../components/project/GeneralRequestDemo';
 
 export default ({ location, data }) => {
-    const { industries, clients, page } = data;
+    const { industries, page } = data;
     const loopIndustry = industries.edges.map(({ node }) => (
         <Card key={node.id} node={node} column="col-lg-6 col-xl-3" item="industry" directory={path.INDUSTRY} />
     ));
-    const loopClient = clients.edges.map(({ node: client }) => <ArticleClient key={client.id} client={client} />);
     return (
         <Layout template={`page page-${page.slug}`} title={page.title} description={logicDescription(page)} location={location}>
             {page && industries.edges.length > 0 && (
@@ -27,11 +25,6 @@ export default ({ location, data }) => {
                     <section className="node-xs-50 node-lg-80 cheat-both">
                         <div className="row gutter-20">{loopIndustry}</div>
                     </section>
-                </Feed>
-            )}
-            {loopClient.length > 0 && (
-                <Feed id="client" space="space-xs-50 space-lg-80" color={5}>
-                    <div className="row align-items-center justify-content-center gutter-50">{loopClient}</div>
                 </Feed>
             )}
             <GeneralRequestDemo />
